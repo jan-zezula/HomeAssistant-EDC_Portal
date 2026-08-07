d78ccaacd2c8e445e5924a598098db2009142e31
tkupka <tomas.kupka@mgm-tp.com>
2026-08-07 16:04:44 +0200

    EDCExporter instance creation fixed

--- appdaemon/apps/edc_importer/edc_importer.py
+++ appdaemon/apps/edc_importer/edc_importer.py
@@ -30,7 +30,7 @@ class EDCImporter(Hass):
 
         self.edcScraper = EdcScraper("/usr/bin/chromedriver", self.args["username"], self.args["password"], self.args["exportGroup"], self.args["dataDirectory"], logger)
         #self.edcExporter = EdcExporter(self.args["dataDirectory"], self.args["basicPrice"], self.args["advancedPrices"], logger, self)
-        self.edcExporter = EdcExporter(self.args["dataDirectory"], '', self.args["basicPrice"], logger, self)
+        self.edcExporter = EdcExporter(self.args["dataDirectory"], self.args["basicPrice"], '', logger, self)
 
         self.listen_event(self.importEdcDataEventHandler, "edc_import")
         self.listen_event(self.importEdcDailyDataEventHandler, "edc_import_daily")
